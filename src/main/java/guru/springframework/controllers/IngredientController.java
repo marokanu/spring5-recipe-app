1f714d666c4d1f21f887b9baa25765f0cd9232c5
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-02-17 17:56:29 +0200

    adding delete ingredient

--- src/main/java/guru/springframework/controllers/IngredientController.java
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -72,6 +72,15 @@ public class IngredientController {
         return "recipe/ingredient/ingredientform";
     }
 
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/{id}/delete")
+    public String deleteRecipe(@PathVariable String recipeId,
+                               @PathVariable String id){
+
+        ingredientService.deleteById(Long.valueOf(recipeId), Long.valueOf(id));
+
+        return "redirect:/recipe/" + recipeId + "/ingredients";
+    }
 
     @PostMapping
     @RequestMapping("recipe/{recipeId}/ingredient")
